3f0b0835a753882302271f0c2d615c270802596d
Vitalii <33979926+vberkunov@users.noreply.github.com>
2022-08-19 14:23:51 +0300

    change env variable

--- data/environment.sh
+++ data/environment.sh
@@ -1,5 +1,5 @@
-export CONFIGUREME_ENVIRONMENT=dev
-export DISTRIBUTEME_PROFILE=dev
+export CONFIGUREME_ENVIRONMENT=dev_vberkunov
+export DISTRIBUTEME_PROFILE=dev_vberkunov
 export JVM_OPTIONS="-Xmx1024M -Xms64M"
 export JAVA_HOME=C:\Program Files\Java\jdk1.8.0_202
 export PATH=$JAVA_HOME/bin:$PATH
